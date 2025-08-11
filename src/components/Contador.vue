7816e4fcd7dfbc1a9c5c4176ab03da1f6f8f2d82
Andre <rmakerandre@gmail.com>
2025-08-11 16:11:02 -0500

    Crear componente "Contador.vue"

---
+++ src/components/Contador.vue
@@ -0,0 +1,29 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+// eslint-disable-next-line vue/multi-word-component-names
+<template>
+  <h1>Hello</h1>
+  <button @click="incrementar">Increment</button>
+  <button @click="decrementar">Decrement</button>
+  <p>Count: {{ count }}</p>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+const incrementar = () => {
+  count.value++
+}
+const decrementar = () => {
+  count.value--
+}
+</script>
+
+<style scoped>
+button {
+  margin-right: 10px;
+}
+p {
+  color: red;
+}
+</style>
