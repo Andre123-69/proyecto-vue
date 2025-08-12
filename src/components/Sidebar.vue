02dc969ec4b274f7bb6b045bb5838d873b715fba
Andre <rmakerandre@gmail.com>
2025-08-12 18:55:26 -0500

    ....

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -2,38 +2,21 @@
 <template>
   <div class="sidebar">
     <h2>Barra lateral</h2>
-    <h3>Nombre: {{ nombre }}</h3>
-    <h3>Email: {{ email }}</h3>
+    <h3>Nombre: {{ registrarStore.nombre }}</h3>
+    <h3>Email: {{ registrarStore.email }}</h3>
     <ul>
       <li><router-link to="/Contador">Contador</router-link></li>
       <li><router-link to="/Lista-De-Tareas">Lista de tareas</router-link></li>
       <li><router-link to="/Registro">Registrar</router-link></li>
+      <li><router-link to="/Productos">Login</router-link></li>
     </ul>
   </div>
 </template>
 
 <script setup>
 import useRegistrarStore from "@/modules/registro/stores/registrarStore.js";
-import { ref, watch } from "vue";
 
 const registrarStore = useRegistrarStore();
-
-const nombre = ref(registrarStore.nombre.value);
-const email = ref(registrarStore.email.value);
-
-watch(
-  () => registrarStore.nombre,
-  (newValue) => {
-    nombre.value = newValue;
-  },
-);
-
-watch(
-  () => registrarStore.email,
-  (newValue) => {
-    email.value = newValue;
-  },
-);
 </script>
 
 <style>
